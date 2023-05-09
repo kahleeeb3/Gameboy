1b02257414ae7ea06fe9e2d0b5ebcecfa0272edd
Caleb Powell <powellcalebm@gmail.com>
2023-05-08 22:28:36 -0500

    updated player animation

--- include/player.h
+++ include/player.h
@@ -8,7 +8,7 @@
 //	+ 64 tiles Metatiled by 4x4 not compressed
 //	Total size: 32 + 2048 = 2080
 //
-//	Time-stamp: 2023-05-08, 17:57:55
+//	Time-stamp: 2023-05-08, 22:22:37
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
--- source/main.c
+++ source/main.c
@@ -1,73 +1,107 @@
 #include <string.h>
 #include <tonc.h>
 #include "player.h"
+#include <stdbool.h>
+
+
+#define PLAYER_SPEED 5
+#define PLAYER_FRAME1 0
+#define PLAYER_FRAME2 16
+#define PLAYER_FRAME3 32
+#define PLAYER_FRAME4 48
+
 
 OBJ_ATTR obj_buffer[128]; // allocate space for 128 sprites
 OBJ_ATTR *player= &obj_buffer[0]; // define the player sprite
+
+int x= 96, y= 32; // default player position
+int frame=0; // frame count to slow polling
 
 
 void input()
 {
 
+	
 	while(1)
 	{
-		vid_vsync(); // wait for VBlank
-		key_poll(); // poll user input
-			
-		u16 frame1 = 0; // first frame
-		u16 frame2 = 16; // second frame
-		u16 frame3 = 32; // third frame
-		u16 frame4 = 48; // fourth frame
-		
-		if(key_hit(KEY_LEFT))
-			player->attr2 = frame1;
-			
-		if(key_hit(KEY_RIGHT))
-			player->attr2 = frame2;
 
-		if(key_hit(KEY_UP))
-			player->attr2 = frame3;
+		// slowing down polling to make the changes visible
+        for (int i = 0; i < 4; i++)
+        {
+			vid_vsync(); // wait for VBlank
+			key_poll(); // poll user input
+		}
 
-		if(key_hit(KEY_DOWN))
-			player->attr2 = frame4;
+
+		// update position data
+		x += PLAYER_SPEED*key_tri_horz();
+		y += PLAYER_SPEED*key_tri_vert();
 		
-		if(key_hit(KEY_A))
-			player->attr2 = (player->attr2 + 16) % 64;
+
+		// if facing right: ch1->attr1 = 0x0000
+		bool facing_right = (player->attr1 & ATTR1_HFLIP) == 0x0000;
+		bool facing_left = !facing_right;
+
+		// if holding up or down, alternate first two sprite frames
+		if(key_held(KEY_DOWN) || key_held(KEY_UP)){
+			player->attr2 = (player->attr2 + 16) % 32; // alternate frames
+		}
+
+
+
+		// if moving left
+		if(key_held(KEY_LEFT)){
+			if(facing_right){
+				// change to frame 4 and then back to frame 1 (skid effect)
+				player->attr2 = PLAYER_FRAME4; // change to frame 4
+				oam_copy(oam_mem, obj_buffer, 1); // update sprite
+				player->attr1 ^= ATTR1_HFLIP; // horizontal flip sprite
+				player->attr2 = PLAYER_FRAME4; // change to frame 1
+			}
+			else{
+				player->attr2 = (player->attr2 + 16) % 32; // alternate frames
+			}
+		}
 		
+		// if moving right
+		if(key_held(KEY_RIGHT)){
+			if(facing_left){
+				// change to frame 4 and then back to frame 1 (skid effect)
+				player->attr2 = PLAYER_FRAME4; // change to frame 4
+				oam_copy(oam_mem, obj_buffer, 1); // update sprite
+				player->attr1 ^= ATTR1_HFLIP; // horizontal flip sprite
+				player->attr2 = PLAYER_FRAME4; // change to frame 1
+			}
+			else{
+				player->attr2 = (player->attr2 + 16) % 32; // alternate frames
+			}
+		}
+
+
+		obj_set_pos(player, x, y);
 		oam_copy(oam_mem, obj_buffer, 1);	// update 1 sprite in obj_buffer
+
 
 	}
 }
 
 
+void create_player_sprite(){
+	memcpy(&tile_mem[4][0], playerTiles, playerTilesLen); // copy sprite tiles
+    memcpy(pal_obj_mem, playerPal, playerPalLen); // copy sprite pallets
+	obj_set_attr(player, ATTR0_4BPP, ATTR1_SIZE_32,0); // set attributes
+	obj_set_pos(player, x, y); // set position
+	oam_copy(oam_mem, obj_buffer, 1);	// update 1 sprite in obj_buffer
+
+}
+
 
 int main()
 {
-	
 	oam_init(obj_buffer, 128); // initialize 128 sprites
-
-	// Display =   Mode 0| Background 0| Enable Objects | OBJ-VRAM as array
-	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_OBJ | DCNT_OBJ_1D;
-	
-	// create sprite
-	memcpy(&tile_mem[4][0], playerTiles, playerTilesLen); // copy sprite tiles
-    memcpy(pal_obj_mem, playerPal, playerPalLen); // copy sprite pallets
-	
-	obj_set_attr(player, 
-		ATTR0_4BPP, // set as 4bpp
-		ATTR1_SIZE_32, // 32x32 pixel sprite
-		0);
-
-	obj_set_pos(player, 96, 32);
-	
-	
-	// update 1 sprite of data in OAM
-	oam_copy(oam_mem, obj_buffer, 1);
-	
-	// while(1);
-
-	// take input
-	input();
-
+	REG_DISPCNT = DCNT_OBJ | DCNT_OBJ_1D; // Enable Objects & OBJ-VRAM as array
+	create_player_sprite(); // create player
+	input(); // get input
+	while(1);
 	return 0;
 }
