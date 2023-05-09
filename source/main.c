c04656462af07a3548af8122b534f0b409b3e75d
Caleb Powell <powellcalebm@gmail.com>
2023-05-08 23:21:15 -0500

    Player animations done

--- source/main.c
+++ source/main.c
@@ -18,13 +18,30 @@ int x= 96, y= 32; // default player position
 int frame=0; // frame count to slow polling
 
 
+void player_throw(){
+	player->attr2 = PLAYER_FRAME3; // change to frame 3
+	oam_copy(oam_mem, obj_buffer, 1); // update sprite
+	// player->attr2 = PLAYER_FRAME4; // change to frame 1
+}
+
+void player_walk(){
+	player->attr2 = (player->attr2 + 16) % 32; // alternate frames 1&2
+}
+
+void player_skid(){
+	// change to frame 4 and then back to frame 1 (skid effect)
+	player->attr2 = PLAYER_FRAME4; // change to frame 4
+	oam_copy(oam_mem, obj_buffer, 1); // update sprite
+	player->attr1 ^= ATTR1_HFLIP; // horizontal flip sprite
+	player->attr2 = PLAYER_FRAME4; // change to frame 1
+}
+
+
 void input()
 {
 
-	
 	while(1)
 	{
-
 		// slowing down polling to make the changes visible
         for (int i = 0; i < 4; i++)
         {
@@ -32,55 +49,53 @@ void input()
 			key_poll(); // poll user input
 		}
 
+		// if press A
+		if(key_is_down(KEY_A)){
+			player_throw();
+		}
 
 		// update position data
 		x += PLAYER_SPEED*key_tri_horz();
 		y += PLAYER_SPEED*key_tri_vert();
 		
-
 		// if facing right: ch1->attr1 = 0x0000
 		bool facing_right = (player->attr1 & ATTR1_HFLIP) == 0x0000;
 		bool facing_left = !facing_right;
 
 		// if holding up or down, alternate first two sprite frames
 		if(key_held(KEY_DOWN) || key_held(KEY_UP)){
-			player->attr2 = (player->attr2 + 16) % 32; // alternate frames
+			player_walk(); // alternate frames
 		}
-
-
 
 		// if moving left
 		if(key_held(KEY_LEFT)){
 			if(facing_right){
-				// change to frame 4 and then back to frame 1 (skid effect)
-				player->attr2 = PLAYER_FRAME4; // change to frame 4
-				oam_copy(oam_mem, obj_buffer, 1); // update sprite
-				player->attr1 ^= ATTR1_HFLIP; // horizontal flip sprite
-				player->attr2 = PLAYER_FRAME4; // change to frame 1
+				player_skid(); // skid animation
 			}
 			else{
-				player->attr2 = (player->attr2 + 16) % 32; // alternate frames
+				player_walk(); // walk animation
 			}
 		}
 		
 		// if moving right
 		if(key_held(KEY_RIGHT)){
 			if(facing_left){
-				// change to frame 4 and then back to frame 1 (skid effect)
-				player->attr2 = PLAYER_FRAME4; // change to frame 4
-				oam_copy(oam_mem, obj_buffer, 1); // update sprite
-				player->attr1 ^= ATTR1_HFLIP; // horizontal flip sprite
-				player->attr2 = PLAYER_FRAME4; // change to frame 1
+				player_skid(); // skid animation
 			}
 			else{
-				player->attr2 = (player->attr2 + 16) % 32; // alternate frames
+				player_walk(); // walk animation
 			}
 		}
 
 
-		obj_set_pos(player, x, y);
-		oam_copy(oam_mem, obj_buffer, 1);	// update 1 sprite in obj_buffer
+		// diagonal movements
+		if( (key_held(KEY_DOWN) && key_held(KEY_RIGHT)) || (key_held(KEY_UP) && key_held(KEY_RIGHT)) || 
+			(key_held(KEY_DOWN) && key_held(KEY_LEFT)) || (key_held(KEY_UP) && key_held(KEY_LEFT)) ){
+				player_walk(); // walk animation
+			}
 
+		obj_set_pos(player, x, y); // set player position
+		oam_copy(oam_mem, obj_buffer, 1);	// update 1 sprite in obj_buffer
 
 	}
 }
