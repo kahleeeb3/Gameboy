8871893b2fc9e066bac842f3f25c051a04759e6e
Caleb Powell <powellcalebm@gmail.com>
2023-05-25 00:28:44 -0500

    moved sprite data to separate file

---
+++ include/spriteData.h
@@ -0,0 +1,44 @@
+#ifndef SPRITESDATA
+#define SPRITESDATA
+
+// GENERAL DEFINES
+#define palLen 32 // all palettes are this size
+
+// PLAYER DEFINES
+#define playerTilesLen 2048
+extern const unsigned int playerTiles[512];
+extern const unsigned int playerPal[8];
+
+// APPLE DEFINES
+#define appleTilesLen 32
+extern const unsigned int appleTiles[8];
+extern const unsigned int applePal[8];
+
+// SQUIRREL
+#define squirrelTilesLen 512
+extern const unsigned int squirrelTiles[128];
+extern const unsigned int squirrelPal[8];
+extern const unsigned int squirrel2Pal[8];
+
+// BUILDING DEFINES
+#define buildingTilesLen 1024
+extern const unsigned int lilly1Tiles[256];
+extern const unsigned int lilly2Tiles[256];
+extern const unsigned int sparks1Tiles[256];
+extern const unsigned int sparks2Tiles[256];
+extern const unsigned int baxter1Tiles[256];
+extern const unsigned int baxter2Tiles[256];
+extern const unsigned int goodrich1Tiles[256];
+extern const unsigned int goodrich2Tiles[256];
+extern const unsigned int hays1Tiles[256];
+extern const unsigned int hays2Tiles[256];
+extern const unsigned int detchon1Tiles[256];
+extern const unsigned int detchon2Tiles[256];
+extern const unsigned int chapel1Tiles[256];
+extern const unsigned int chapel2Tiles[256];
+extern const unsigned int center1Tiles[256];
+extern const unsigned int center2Tiles[256];
+extern const unsigned int buildingsPal[8];
+
+
+#endif /* SPRITESDATA */
--- include/sprites.h
+++ include/sprites.h
@@ -1,99 +1,56 @@
-#include <tonc.h>
-
-
 #ifndef SPRITES
 #define SPRITES
 
-// boarders
-#define MAP_MAX_X 270
-#define MAP_MAX_Y 260
-#define PLAYER_MAX_X 208
-#define PLAYER_MIN_X 0
-#define PLAYER_MAX_Y 128
-#define PLAYER_MIN_Y 0
+#include <tonc.h> // GBA library
 
-// Define Max Quantities
-#define PLAYER_MAX      1
-#define APPLE_MAX 		5
-#define SQUIRREL_MAX	50
-#define BUILDINGS_MAX 	16
+// max values
+#define PLAYER_MAX              1
+#define APPLE_MAX               5
+#define SQUIRREL_MAX            50
+#define BUILDINGS_MAX           16
 
-// These will be moved to input later
-#define PLAYER_SPEED 		5
-#define APPLE_SPEED 		7
-#define SQUIRREL_SPEED 		1
-#define PLAYER_HIT_BOX		8
-#define SQUIRREL_HIT_BOX 	4
+// tile values
+#define PLAYER_TILE_ID          0
+#define APPLE_TILE_ID           64
+#define SQUIRREL_TITLE_ID	    66
+#define BUILDING_TILE_ID        82
 
-// Tile index for sprite frames
-#define PLAYER_FRAME1 		0
-#define PLAYER_FRAME2		16
-#define PLAYER_FRAME3		32
-#define PLAYER_FRAME4		48
-#define APPLE_FRAME1 		64
-#define SQUIRREL_FRAME1 	66
-#define SQUIRREL_FRAME2		70
-#define SQUIRREL_FRAME3		74
-#define SQUIRREL_FRAME4		78
-#define BUILDING_FRAME1     82
-
-// Palette index for spites
-#define PLAYER_PAL_OFFSET 		0
-#define APPLE_PAL_OFFSET 		16
-#define SQUIRREL_PAL_OFFSET 	32
-#define BUILDINGS_PAL_OFFSET    48
-
-// define palette banks
+// palette values
 #define PLAYER_PAL_BANK 		0
-#define APPLE_PAL_BANK			1
-#define SQUIRREL_PAL_BANK 		2
+#define APPLE_PAL_BANK          1
+#define SQUIRREL_PAL_BANK       2
 #define BUILDINGS_PAL_BANK 		4
 
-
-// Define Sprite Sizes
-#define PLAYER_SIZE 		32
-#define APPLE_SIZE 			8
-#define SQUIRREL_SIZE 		16
-#define BUILDINGS_SIZE      -1
-
-// all sprites will have a palette size of 32
-// because there is 8 colors at 4 bytes per pixel
-// meaning each color is 4 bytes (16 unique colors)
-#define PalLen 32
-
-// Sprite Tile Values
-#define playerTilesLen 2048
-#define appleTilesLen 32
-#define squirrelTilesLen 512
-#define buildingTilesLen 1024
-
+// sprite struct
 typedef struct{
-	OBJ_ATTR *mem_addr; // pointer to location in object mem
+	OBJ_ATTR *mem_addr; // pointer to sprite location in object mem
 	int throwing; // bool value for if the player sprite threw an apple
 	int hidden; // bool for if the sprite is visible or not
-	int size; // number of pixels in the horizontal direction
     int active; // if the sprite is an active part of the game (visibility)
-	int start_tile; // tile number of the first tile
+	int size; // number of pixels in the horizontal direction
+	int start_tile; // starting index in tile memory
+	int curr_tile; // current index in tile memory (for animation)
 	int pal_bank; // which palette bank
-	int curr_frame; // which animation frame the sprite is currently on
 	int dir_facing; // what direction the sprite is facing
 	int x_pos; // current x position
 	int y_pos; // current y position
-	int index; // which sprite occurrence this is (for duplicate sprites)
 	int target; // which sprite are they targeting (for squirrels only)
 	int hbx; // hit box x size
 	int hby; // hit box y size
+	int index; // which sprite occurrence this is (for duplicate sprites)
 }Sprite;
 
-void copy_tiles(const unsigned int *tileSet, int tileSetStart, int tileSetLen);
-void copy_palette(const unsigned int *palSet, int palSetStart, int palSetLen);
-Sprite *mkSpriteStruct(OBJ_ATTR *obj_buffer_location, int size, int tile_id, int pal_bank, int active, int x, int y);
+extern OBJ_ATTR obj_buffer[128]; // allocate space for 128 sprites
+extern OBJ_AFFINE *obj_aff_buffer; // buffer for object affine
+extern Sprite *player; // global pointer to the player sprite
+extern Sprite* apples[APPLE_MAX]; // array of building sprite pointers
+extern Sprite* squirrels[SQUIRREL_MAX]; // array of building sprite pointers
+extern Sprite* buildings[BUILDINGS_MAX]; // array of building sprite pointers
+
 void copy_sprite_data();
-void scale_sprite(Sprite *sprite, OBJ_AFFINE *obj_aff_buffer_location, int obj_affine_num, int scale_factor);
+void set_player_attributes();
+void set_apple_attributes();
+void set_squirrel_attributes();
+void set_building_attributes();
 
-void set_player_attributes(OBJ_ATTR *obj_buffer, Sprite** players);
-void set_apples_attributes(OBJ_ATTR *obj_buffer, Sprite** apples);
-void set_squirrels_attributes(OBJ_ATTR *obj_buffer, Sprite** squirrels);
-void set_buildings_attributes(OBJ_ATTR *obj_buffer, OBJ_AFFINE *obj_aff_buffer, Sprite** buildings);
-
-#endif // SPRITES
+#endif /* SPRITES */
